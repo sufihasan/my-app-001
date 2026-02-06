518515e0f165059477784274cfe0c3fc1510d1ab
sufihasan <hasan6nh@gmail.com>
2026-02-06 11:47:17 +0600

    make compare password part in route.ts

--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -1,10 +1,28 @@
 import { mongoConnect } from "@/lib/mongoConnect";
+import { error } from "console";
 import { NextRequest, NextResponse } from "next/server";
+import *as bcrypt from "bcrypt";
 
 async function POST(req: NextRequest, res: NextResponse) {
     try {
         const { client, db } = await mongoConnect();
         const { email, password } = await req.json();
+
+        // find user
+        const user = await db.collection("users").findOne({ email });
+
+        if (!user) {
+            return NextResponse.json({ error: "Invalid credentials" }, { status: 401 });
+        }
+
+        // compare password
+        const isPasswordValid = await bcrypt.compare(password, user.password)
+
+        if (!isPasswordValid) {
+            return NextResponse.json({ error: "Invalid credentials" }, { status: 401 });
+        }
+
+
     }
     catch (err) {
         console.log(err);
